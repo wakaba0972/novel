a70746f6b8d3ebf179b2d6b9a861415c6b93272e
wakaba <benny.wu2199@gmail.com>
2022-03-30 14:19:51 +0800

    url finish

--- crawler.js
+++ crawler.js
@@ -4,6 +4,19 @@ var urls = {
     'k': {
         'n': "https://www.kadokawa.com.tw/product_list297.htm",
         'm': "https://www.kadokawa.com.tw/product_list298.htm"
+        },
+    't': {
+        'n': "https://www.tongli.com.tw/NovelDetail.aspx",
+        'm': "https://www.tongli.com.tw/webpagebooks.aspx"
+        },
+    's': {
+        'n': "https://www.spp.com.tw/productlist?item1=02",
+        'm': "https://www.spp.com.tw/productlist?item1=01"
     }
 }
 
+module.exports.getK = function(type){
+    return new Promise(function(resolve, reject){
+        {url: urls.k[type]}
+    }
+)}
