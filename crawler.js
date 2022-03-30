35d6e761e6fe332059e305ad1f4f8cb35098d384
wakaba <benny.wu2199@gmail.com>
2022-03-30 14:12:44 +0800

    first commit

---
+++ crawler.js
@@ -0,0 +1,9 @@
+axios = require('axios')
+
+var urls = {
+    'k': {
+        'n': "https://www.kadokawa.com.tw/product_list297.htm",
+        'm': "https://www.kadokawa.com.tw/product_list298.htm"
+    }
+}
+
