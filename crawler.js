33dac46271026f32ef8f27b67d4edb73c801146b
wakaba0972 <benny.wu2199@gmail.com>
2022-03-31 00:12:23 +0800

    sdsd

--- crawler.js
+++ crawler.js
@@ -14,7 +14,7 @@ const URLS = {
     }
 }
 const COOKIE = "_fbp=fb.2.1648649757903.618830472; incap_ses_934_1045467=V22cFARPKUPd69Wiljz2DCRmRGIAAAAA7YejYg924PwjxieuXNNbWg==; PHPSESSID=ulank8ip4f80ea8lhj2pklecb6; visid_incap_1045467=9pxVAl+8SsCcIo8iySguzSRmRGIAAAAAQkIPAAAAAACA+EWjAXIomgaPOxCwWQlb8Q/TLOZN8FHO; _ga=GA1.3.175109404.1648649778; _gid=GA1.3.1869672470.1648649778"
-const COOKIEs = "incap_ses_934_1045467=V22cFARPKUPd69Wiljz2DCRmRGIAAAAA7YejYg924PwjxieuXNNbWg==;"
+const COOKIEs = "incap_ses_934_1045467=5++pGmY+qjVo8PGiljz2DAiARGIAAAAAO3PDQry3V6nOMaXMcX8Adg==;"
 const HEADERS = {
     //'content-type': 'application/x-www-form-urlencoded',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
